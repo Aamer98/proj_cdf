47d7bcad28e08190676f1031c050b47680875914
Aamer Abdul Rahman <ar.aamer@gmail.com>
2021-08-20 18:20:43 +0400

    Added CDFSL files
    First upload

---
+++ configs.py
@@ -0,0 +1,19 @@
+save_dir                    = '/content/logs/'
+
+miniImageNet_path           	= '/content/miniImagenet'
+DTD_path 						= '/content/dtd/images/'
+
+ISIC_path = "/ssd/data/ISIC"
+ChestX_path = "/ssd/data/chestX"
+CropDisease_path = "/ssd/data/CropDiseases"
+EuroSAT_path = "/content/2750"
+
+EuroSAT_unlabelled_path = '/content/eurosat_unlabel'
+
+
+num_epochs = 200
+batch_size = 16
+
+start_epoch = 0
+stop_epoch = 500
+save_freq = 50
